86848d2e6ca435a4d76af90ac5100ed1a5a15771
Stefan Otte <stefan.otte@gmail.com>
2024-02-22 17:18:05 +0100

    feat: add layer lock via right pinky (top right)

--- keyboards/crkbd/keymaps/sotte/rules.mk
+++ keyboards/crkbd/keymaps/sotte/rules.mk
@@ -1,8 +1,14 @@
+# https://getreuer.info/posts/keyboards/achordion/index.html
 # SRC += features/achordion.c
+
+# https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
 SRC += features/custom_shift_keys.c
+
+# https://getreuer.info/posts/keyboards/layer-lock/index.html
+SRC += features/layer_lock.c
+
 SRC += features/oneshot.c
 
-CONVERT_TO=promicro_rp2040
 MOUSEKEY_ENABLE = yes
 
 # https://docs.qmk.fm/#/feature_caps_word
@@ -10,3 +16,5 @@ CAPS_WORD_ENABLE = yes
 
 # https://docs.qmk.fm/#/feature_repeat_key
 REPEAT_KEY_ENABLE = yes
+
+CONVERT_TO=promicro_rp2040
