34c8d2feef3025826aadc50d624739a46e60b723
Stefan Otte <stefan.otte@gmail.com>
2025-07-12 09:33:24 +0200

    _NUM2: switch 0 and 9 position

--- keyboards/crkbd/keymaps/sotte_exp_achordion/keymap.c
+++ keyboards/crkbd/keymaps/sotte_exp_achordion/keymap.c
@@ -174,8 +174,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NUM2] = LAYOUT_split_3x6_3(
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_5,    KC_6,    KC_7,    KC_8, _______,
-    _______, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT,  KC_SPC,                         KC_9,    KC_1,    KC_2,    KC_3,    KC_4, _______,
-    _______, KC_LALT, KC_LGUI, KC_LCTL,  KC_SPC, XXXXXXX,                         KC_0,    KC_0, KC_COMM,  KC_DOT, KC_UNDS, _______,
+    _______, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT,  KC_SPC,                         KC_0,    KC_1,    KC_2,    KC_3,    KC_4, _______,
+    _______, KC_LALT, KC_LGUI, KC_LCTL,  KC_SPC, XXXXXXX,                         KC_0,    KC_9, KC_COMM,  KC_DOT, KC_UNDS, _______,
                                         _______, _______, _______,    _______, _______, _______
     //                                                    ^^^^^^^
   )
